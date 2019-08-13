9f46c66fb635d9c1b3dac8538406a109d75f746b
Chia-Tse Weng <wengyagami@hotmail.com>
2019-08-13 19:47:28 +0900

    add picture loader function to character

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_095918) do
+ActiveRecord::Schema.define(version: 2019_08_13_103015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_08_13_095918) do
     t.string "real_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
