7d197b16b3f5f564153c2f3ccbc5db79fe8e9433
Jordan Luong <jordanluong@live.com>
2019-08-15 14:59:35 +0900

    finished home

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :characters, only: [:new]
   resources :characters, only: [:index, :show, :create, :destroy] do
     resources :bookings, only: [:new, :create]
   end
 
   resources :bookings , only: [:index]
-  resources :users, only:[:show] do
-    resources :characters, only: [:new]
-  end
+  resources :users, only:[:show]
 end
