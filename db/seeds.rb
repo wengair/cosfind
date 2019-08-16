6d101b0e14a0f8e31868c93c9ef3dfa3d9555bf1
Jordan Luong <jordanluong@live.com>
2019-08-16 18:05:08 +0900

    fixed login redirect

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,62 @@ Booking.destroy_all
 Character.destroy_all
 User.destroy_all
 
-# create users
+# create random users
 5.times do |counter|
   counter += 1
   User.create!(email: "test#{counter}@gmail.com", password: "123456", name: "name#{counter}", real_name: Faker::Name.name)
 end
 
-# create characters
-User.all.each do |user|
-  2.times do
-    character = Character.new(name: "#{Faker::Superhero.unique.prefix} #{Faker::Superhero.unique.descriptor}", price: Faker::Number.within(range: 100..1000), description: Faker::Lorem.paragraph, user: user)
-
-    character.remote_photo_url = 'https://i.imgur.com/gZ9VxN9.jpg'
-    character.save!
-  end
+# assign 3 random chars to random users
+5.times do |user|
+  character = Character.new(name: "#{Faker::Superhero.unique.prefix} #{Faker::Superhero.unique.descriptor}", price: Faker::Number.within(range: 100..1000), description: Faker::Lorem.paragraph, user: User.all.sample)
+  character.remote_photo_url = 'https://i.imgur.com/gZ9VxN9.jpg'
+  character.save!
 end
 
-print "Added 5 users and assigned 10 characters randomly"
+# create testUser
+testUser = User.create(email: "aaa@hotmail.com", password: "testtest")
+
+# assign 5 random chars to testUser
+5.times do
+  character = Character.new(name: "#{Faker::Superhero.unique.prefix} #{Faker::Superhero.unique.descriptor}", price: Faker::Number.within(range: 100..1000), description: Faker::Lorem.paragraph, user: testUser)
+  character.remote_photo_url = 'https://i.imgur.com/gZ9VxN9.jpg'
+  character.save!
+end
+
+# assign 3 pending bookings to testUser
+counter = 0
+
+3.times do
+  booking = Booking.new(location: "Shinjuku", date: Date.today, comment: "test")
+  booking.user = testUser
+  booking.character = Character.all[counter]
+  booking.save!
+  counter += 1
+end
+
+# assign 3 accepted bookings to testUser
+3.times do
+  booking = Booking.new(status: "Accepted", location: "Shinjuku", date: Date.today, comment: "test")
+  booking.user = testUser
+  booking.character = Character.all[counter]
+  booking.save!
+  counter += 1
+end
+
+# assign 3 historical bookings to testUser
+3.times do
+  booking = Booking.new(status: ["Accepted", "Denied", "Cancelled"].sample, location: "Shinjuku", date: Date.today - 10, comment: "test")
+  booking.user = testUser
+  booking.character = Character.all[counter]
+  booking.save!
+  counter += 1
+end
+
+# assign random booking from testUser to another user
+booking = Booking.new(location: "Shinjuku", date: Date.today, comment: "test")
+booking.user = User.all.sample
+booking.character = Character.first
+booking.save!
+
+print "Seed completed successfully"
